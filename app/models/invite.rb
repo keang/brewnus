a3e057e9795c3be2ad18fb7f5e19c6571771c79e
kaka <skeang@gmail.com>
2014-02-02 17:51:02 +0800

    delete invite

--- app/models/invite.rb
+++
@@ -1,4 +0,0 @@
-class Invite < ActiveRecord::Base
-  belongs_to :inviter
-  belongs_to :invitee
-end
--- spec/factories/invites.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :invite do
-    inviter nil
-    invitee nil
-    code "MyString"
-  end
-end
